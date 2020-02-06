cd9dbde07042893616b70be1d6152700d7af3c18
guanarp <60052556+guanarp@users.noreply.github.com>
2020-02-06 18:13:04 -0300

    Update Ejercicio 3 FizzBuzz.py
    optimizado

--- Ejercicio 3 FizzBuzz.py
+++ Ejercicio 3 FizzBuzz.py
@@ -1,9 +1,11 @@
-for i in range(1,101): #loop para imprimir los numeros del 1 al 100
-    if i%3 == 0 and i%5 != 0: #que sea multiplo de 3 y no de 5
-        print('Fizz')
-    elif i%5 == 0 and i%3 != 0: #que sea de 5 y no de 3
-        print('Buzz')
-    elif i%3 == 0 and i%5 == 0: #que sea de 3 Y de 5
-        print('FizzBuzz')
-    else: #Cuando no es multiplo de 3 ni de 5 y se imprime de forma normal
-        print(i)           
+def impresor(min, max, mod1, mod2):
+    for i in range(min,max+1): #loop para imprimir los numeros del 1 al 100
+        if i% mod1 == 0 and i% mod2 != 0: #que sea multiplo de 3 y no de 5
+            print('Fizz')
+        elif i% mod2 == 0 and i% mod1 != 0: #que sea de 5 y no de 3
+            print('Buzz')
+        elif i% mod1 == 0 and i% mod2 == 0: #que sea de 3 Y de 5
+            print('FizzBuzz')
+        else: #Cuando no es multiplo de 3 ni de 5 y se imprime de forma normal
+            print(i) 
+impresor(1,100,3,5)     
